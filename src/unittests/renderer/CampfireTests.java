504dfa92ad4a2946fcee10ad390d0d022e1023e9
Ariel Yekutiel <arielojelo@gmail.com>
2022-06-02 16:38:05 +0300

    added mountains and sky

--- src/unittests/renderer/CampfireTests.java
+++ src/unittests/renderer/CampfireTests.java
@@ -24,22 +24,50 @@ public class CampfireTests {
         Camera camera = new Camera(new Point(0, -50, 50), new Vector(0, 1, 0), new Vector(0, 0, 1)) //
                 .setVPSize(150, 150).setVPDistance(50);
         scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255), new Double3(0.1)));
-        scene.lights.add( //
-                new SpotLight(new Color(255, 255, 255), new Point(-30, -50, 50), new Vector(0,1,0)) //
-                        .setKl(0.0004).setKq(0.00006));
 
-        scene.geometries.add(new Tree(new Point(-50, 20, 80), 70, 40, 20));
-        scene.geometries.add(new Tree(new Point(0, 20, 80), 90, 40, 30));
+       // scene.lights.add( //
+         //       new SpotLight(new Color(255, 255, 255), new Point(-30, -50, 50), new Vector(0,1,0)) //
+           //             .setKl(0.0004).setKq(0.00006));
+
+
+        //trees
+        scene.geometries.add(new Tree(new Point(-55, 10, 30), 35, 20, 20));
+        scene.geometries.add(new Tree(new Point(65, 15, 30), 45, 22, 30));
+
+        //sky
+        Triangle sky1 = new Triangle(new Point(-850,500, -100), new Point(850, 500, -100), new Point(-850, 500, 900));
+        Triangle sky2 = new Triangle(new Point(850,500, -100), new Point(-850, 500, 900), new Point(850, 500, 900));
+        Material sky = new Material().setKd(0.01);
+        scene.geometries.add(sky1.setEmission(new Color(12, 20, 69)).setMaterial(sky));
+        scene.geometries.add(sky2.setEmission(new Color(12, 20, 69)).setMaterial(sky));
+
+
+        //ground
         Triangle surface1 = new Triangle(new Point(-100,0, -40), new Point(100, 0, -40), new Point(850, 500, -40));
         Triangle surface2 = new Triangle(new Point(-100,0, -40), new Point(-850, 400, -40), new Point(850, 500, -40));
         Material surface = new Material().setKd(0.2);
         scene.geometries.add(surface1.setEmission(new Color(58, 46, 39)).setMaterial(surface));
         scene.geometries.add(surface2.setEmission(new Color(58, 46, 39)).setMaterial(surface));
-        //Sphere moon = new Sphere(new Point(350, 300, 300), 30);
+
+
+
+        //mountains
+        Triangle mountain1 = new Triangle(new Point(-700, 400, -40), new Point(0, 400, -40), new Point(-350, 400, 300));
+        Triangle mountain2 = new Triangle(new Point(-200, 400, -40), new Point(400, 400, -40), new Point(100, 400, 500));
+        Triangle mountain3 = new Triangle(new Point(200, 400, -40), new Point(1000, 400, -40), new Point(600, 400, 350));
+        Triangle mountain4 = new Triangle(new Point(-800, 400, -40), new Point(-800, 400, 400), new Point(-400, 400, -40));
+        scene.geometries.add(mountain1.setEmission(new Color(42, 51, 60)).setMaterial(sky));
+        scene.geometries.add(mountain2.setEmission(new Color(23, 30, 35)).setMaterial(sky));
+        scene.geometries.add(mountain3.setEmission(new Color(31, 38, 48)).setMaterial(sky));
+        scene.geometries.add(mountain4.setEmission(new Color(35, 32, 50)).setMaterial(sky));
+
+        //moon
         Circle moon = new Circle(new Point(350, 300, 300), new Vector(0, -1, 0), 50);
         scene.geometries.add(moon.setEmission(new Color(255,243,109)));
-        Ray r = new Ray(new Point(40, 20, 0), new Vector(2, 1, 0));
-        scene.geometries.add(new TreeLog(r, 10, 150));
+
+        //tree logs
+       // Ray r = new Ray(new Point(30, 0, 0), new Vector(2, 1, 0));
+        //scene.geometries.add(new TreeLog(r, 3, 45));
 
         ImageWriter imageWriter = new ImageWriter("pyramid", 500, 500);
         camera.setImageWriter(imageWriter) //
